18776aed940dbb45ba4461151b94936d982d7d9c
Ricardo Oliveira <rfo20112004@gmail.com>
2024-04-15 19:50:24 +0100

    integration with navbar

--- app_prototype/lib/pages/profile_page.dart
+++ app_prototype/lib/pages/profile_page.dart
@@ -31,9 +31,9 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
